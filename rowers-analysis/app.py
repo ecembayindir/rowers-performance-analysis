c05ad5fe02eda6cfa82643d34271c77016e94c96
Ecem <63916469+ecembayindir@users.noreply.github.com>
2024-10-26 17:22:22 +0200

    app.py

--- rowers-analysis/app.py
+++ rowers-analysis/app.py
@@ -12,11 +12,11 @@ import statsmodels.api as sm
 from statsmodels.formula.api import ols
 
 # Load your processed data
-df = pd.read_csv('/rowers-analysis/merged_series_data.csv')
-anova_results_df = pd.read_csv('/rowers-analysis/anova_results.csv')
-coef_df = pd.read_csv('/rowers-analysis/ols_coefficients.csv')
-model_summary_df = pd.read_csv('/rowers-analysis/ols_summary.csv')
-noc = pd.read_csv('/rowers-analysis/data_externes_aviron.csv')
+df = pd.read_csv('rowers-analysis/anova_results.csv')
+anova_results_df = pd.read_csv('rowers-analysis/anova_results.csv')
+coef_df = pd.read_csv('rowers-analysis/ols_coefficients.csv')
+model_summary_df = pd.read_csv('rowers-analysis/ols_summary.csv')
+noc = pd.read_csv('rowers-analysis/data_externes_aviron.csv')
 
 # Sidebar for tab selection
 st.sidebar.title("🏁 Navigation")
